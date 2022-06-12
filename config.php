fa937e258bb719c919300c5257a9222f438073fe
afnan <afnanhassan96@gmail.com>
2022-06-12 12:18:47 +0300

    A

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://aswaaq.xyz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://aswaaq.xyz/');
+
+// DIR
+define('DIR_APPLICATION', '/home/openjohina/aswaaq.xyz/catalog/');
+define('DIR_SYSTEM', '/home/openjohina/aswaaq.xyz/system/');
+define('DIR_IMAGE', '/home/openjohina/aswaaq.xyz/image/');
+define('DIR_STORAGE', '/home/openjohina/storagefree/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'openjohina_freemarket');
+define('DB_PASSWORD', 'free14789');
+define('DB_DATABASE', 'openjohina_freemarket');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
